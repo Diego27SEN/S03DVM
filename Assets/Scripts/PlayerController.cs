5f00daa1337c7d1974d6fb359dccfaea2507d01c
Lionel-Villanueva <banitessebastian2decundaria@gmail.com>
2026-03-31 12:15:05 -0500

    =)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,26 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
 
 
+
     public float moveSpeed = 5f;
     public float rotationSpeed = 200f;
     public float verticalVelocity = 0;
     public float jumpForce = 10;
 
+    public float pushForce = 4;
 
-    [SerializeField]private Vector2 moveInput;
+    private bool IsDashing;
+    public float dashForce;
+    public float dashDuration = 0.2f;
+    private float dashTimer;
+
+
+
+
+    [SerializeField] private Vector2 moveInput;
+
+
+
 
     private void Awake()
     {
@@ -25,25 +38,25 @@ public class PlayerController : MonoBehaviour
     {
         inputs.Enable();
 
-        inputs.Player.Move.performed += ctx =>  moveInput = ctx.ReadValue<Vector2>();
+        inputs.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         inputs.Player.Move.canceled += ctx => moveInput = Vector2.zero;
 
 
         inputs.Player.Jump.performed += OnJump;
 
-        
+        inputs.Player.Sprint.performed += OnDash;
+
+
 
     }
     void Start()
     {
 
     }
-
-    
     void Update()
     {
 
-         OnMove();
+        OnMove();
         //OnSimpleMove();
     }
 
@@ -54,12 +67,25 @@ public class PlayerController : MonoBehaviour
 
         verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
-        if(controller.isGrounded && verticalVelocity < 0)
+        if (controller.isGrounded && verticalVelocity < 0)
             verticalVelocity = -2f;
 
 
         moveDir.y = verticalVelocity;
-        print(moveDir);
+
+        if (IsDashing)
+        {
+            moveDir = transform.forward * dashForce * (dashTimer / dashDuration);
+
+            dashTimer -= Time.deltaTime;
+
+            if (dashTimer <= 0)
+                IsDashing = false;
+        }
+
+
+
+
         controller.Move(moveDir * Time.deltaTime);
     }
 
@@ -69,23 +95,28 @@ public class PlayerController : MonoBehaviour
 
         verticalVelocity = jumpForce;
     }
-
-
-
-
-
-
-
-
-
-
-
-
     public void OnSimpleMove()
     {
         transform.Rotate(Vector3.up * moveInput.x * rotationSpeed * Time.deltaTime);
-        Vector3 moveDir = transform.forward * moveSpeed * moveInput.y ;
+        Vector3 moveDir = transform.forward * moveSpeed * moveInput.y;
         controller.SimpleMove(moveDir);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+
+        Vector3 pushDir = (hit.transform.position - transform.position).normalized;
+
+        if (hit.rigidbody != null && hit.rigidbody.linearVelocity == Vector3.zero)
+        {
+            print(hit.gameObject.name);
+            hit.rigidbody.AddForce(pushDir * pushForce, ForceMode.Impulse);
+        }
+    }
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        IsDashing = true;
+        dashTimer = dashDuration;
     }
 
 }
